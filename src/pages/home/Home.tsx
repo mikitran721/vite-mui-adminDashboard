1d79895ad8b8a14e987f8786f57a8773b8e2f9d9
MikiTran <mikitran721@gmail.com>
2023-10-04 17:38:30 +0700

    pie chart box - box4

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import BarChartBox from "../../components/barChartBox/BarChartBox";
 import ChartBox from "../../components/chartBox/ChartBox";
+import PieChartBox from "../../components/pieChartBox/PieChartBox";
 import {
   barChartBoxRevenue,
   barChartBoxVisit,
@@ -23,7 +24,9 @@ const Home = () => {
       <div className="box box3">
         <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4"></div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
       <div className="box box5">
         <ChartBox {...chartBoxConversion} />
       </div>
